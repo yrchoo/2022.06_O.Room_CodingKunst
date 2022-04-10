02d2efe4323d2fa2d7a542d91c5f67f76f39a1dc
yrchoo <yeahrin0000@hansung.ac.kr>
2022-04-10 16:26:18 +0900

    04.10 yerin
    change customizeScene

--- VRMeetingProgram/Assets/Scripts/CustomizeSkinButton.cs
+++ VRMeetingProgram/Assets/Scripts/CustomizeSkinButton.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomizeSkinButton : MonoBehaviour
 {
     private bool IsClick;
     public int skinN = 0;
     private int skinData;
+    public Toggle toggle;
 
     // Start is called before the first frame update
     void Start()
     {
         IsClick = false;
         skinData = GameObject.Find("MyCharacter").GetComponent<GetCustomizeData>().skinData;
+        CheckToggle();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void CheckToggle(){
+        if(skinN == skinData){
+            toggle.isOn = true;
+        }
+        else{
+            toggle.isOn = false;
+        }
     }
 
     public void ChangeSkin(bool clicked){
