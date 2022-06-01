f2118903d36da88435d94c4c13ee85d6e8bd5706
khl3846 <57594295+khl3846@users.noreply.github.com>
2022-06-01 12:39:12 +0900

    fixed ConferenceRoom1
    fixed ConferenceRoom1

--- VRMeetingProgram/Assets/Scripts/cshMute.cs
+++ VRMeetingProgram/Assets/Scripts/cshMute.cs
@@ -11,19 +11,27 @@ public class cshMute : MonoBehaviour
     public Recorder recorder;
     public Image muteImage;
     public PhotonView pv;
+    private bool isMute;
 
     private int count;
 
     [PunRPC]
     void mute()
     {
-        muteImage.enabled = true;
+        if (isMute)
+        {
+            muteImage.enabled = true;
+        }
+        
     }
 
     [PunRPC]
     void mute2()
     {
-        muteImage.enabled = false;
+        if (!isMute)
+        {
+            muteImage.enabled = false;
+        }    
     }
 
     // Start is called before the first frame update
@@ -32,27 +40,39 @@ public class cshMute : MonoBehaviour
         pv = GetComponent<PhotonView>();
         count = 0;
         recorder = GameObject.Find("VoiceController").GetComponent<Recorder>();
+        //muteImage = GameObject.Find("GameManager").GetComponent<GameManager>().myPlayer.transform.Find("Mute").GetComponent<Image>();
         muteImage.enabled = false;
+        //muteImage.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (pv.IsMine)
         {
-            if(count % 2 == 0)
+            if (Input.GetKeyDown(KeyCode.M))
             {
-                recorder.IsRecording = false;
-                pv.RPC("mute", RpcTarget.All);
-                count++;
-            } else
-            {
-                recorder.IsRecording = true;
-                pv.RPC("mute2", RpcTarget.All);
-                count++;
+                if (count % 2 == 0)
+                {
+                    recorder.IsRecording = false;
+                    isMute = true;
+                    pv.RPC("mute", RpcTarget.AllBuffered);
+                    //mute();
+                    //PhotonNetwork.Instantiate("Mute", transform.position, transform.rotation).transform.parent = transform;
+                    count++;
+                }
+                else
+                {
+                    recorder.IsRecording = true;
+                    isMute = false;
+                    pv.RPC("mute2", RpcTarget.AllBuffered);
+                    //mute2();
+                    count++;
+                }
+
             }
-            
         }
+        
     }
 
 
